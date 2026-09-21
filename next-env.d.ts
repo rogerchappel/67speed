cf7a6450000c496ed65198c14baee9c8d8bf39a6
Roger Chappel <miscanalysis@gmail.com>
2026-09-21 14:01:21 +1000

    chore: add missing TypeScript references to next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,4 @@ import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+import "./.next/types/root-params.d.ts";
