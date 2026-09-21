8d9f595d7b7621876fce17f592225502856f867b
Roger Chappel <miscanalysis@gmail.com>
2026-09-21 17:44:58 +1000

    chore: remove redundant import from next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -5,4 +5,3 @@ import "./.next/types/root-params.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-import "./.next/types/root-params.d.ts";
